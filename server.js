b0ceff21748578e1b4c0f27db244c06aa8a55ea8
svakil <smv2@protonmail.com>
2019-07-17 15:51:46 -0400

    Initial commit

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const sum = require('./utils/sum');
+const app = express();
+const PORT = process.env.PORT || 5000
+
+app.get('/', (req, res) => {
+    let total = sum(100, 1);
+    res.send('Hello NodeJs automation on Heroku ' + total);
+});
+
+app.listen(PORT, () => {
+    /* eslint-disable no-console */
+    console.log(`Example app listening on port ${PORT}!`);
+    /* eslint-enable no-console */
+});
